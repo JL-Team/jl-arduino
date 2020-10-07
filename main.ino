93ca0bcf1cebbaee72508fb44952568d8d4c6a14
BChen233 <60862358+BChen233@users.noreply.github.com>
2020-10-07 15:38:34 +0800

    Add files via upload

--- main.ino
+++ main.ino
@@ -1,22 +1,37 @@
+#include <Wire.h> 
+#include <RSCG12864B.h>
+int nzhi = 0;
+int nzmi = 0;
+int xh=1,xm=0;
+int tpressed=0;
+char nzm[61][3]={"00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60",};
+char nzh[25][3]={"00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"};
 void setup()
 {
   Serial.begin(9600);
-  pinMode(11,INPUT);
-  pinMode(12,OUTPUT);
-  pinMode(8,INPUT);
-  pinMode(9,INPUT);
-  pinMode(10,INPUT);
-  pinMode(13,OUTPUT);
+  RSCG12864B.begin();
+  RSCG12864B.clear();
+  RSCG12864B.brightness(100);
+  //Serial.println("AT+ROLE0");
+  //delay(5000);
+  //Serial.println("AT+ADDR");
+  pinMode(11,INPUT);//
+  pinMode(12,OUTPUT);//
+  pinMode(8,INPUT);//按钮1
+  pinMode(9,INPUT);//按钮2
+  pinMode(10,INPUT);//按钮3
+  pinMode(13,OUTPUT);//
+  JL_ScreenSetup();
 }
-int JL_BUTT()
+void JL_ScreenSetup()
 {
-  switch(not(digitalRead(8))+not(digitalRead(9))*2+not(digitalRead(10))*4)
-  {
-    case 1:return 1;
-    case 2:return 2;
-    case 4:return 3;
-    default:return 0;
-  }
+  RSCG12864B.print_string_16_xy(2,16,"JL-Clock.");
+  RSCG12864B.print_string_16_xy(30,32,"By-JL-Team");
+  delay(2000);
+  RSCG12864B.clear();
+  RSCG12864B.print_string_16_xy(5,16,"WelCome!");
+  delay(1000);
+  RSCG12864B.clear();
 }
 int JL_ULT()
 {
@@ -27,7 +42,53 @@ int JL_ULT()
   digitalWrite(12,LOW);
   return (pulseIn(11,1,10000L) / 59);
 }
+void JL_Set_Time()//设置时钟
+{
+    int xh=1,xm=0;
+    RSCG12864B.clear();
+    Serial.println("GO");
+    int hpressed=0,mpressed=0;
+    RSCG12864B.print_string_12_xy(40,25,nzh[xh]);//显示
+    RSCG12864B.print_string_12_xy(55,25,nzm[xm]);
+    while(1)
+    { 
+      if(hpressed==0 && digitalRead(9)==0)//设置小时
+      {
+         hpressed = 1;
+         xh++;
+         if(xh==24) xh=0;
+         RSCG12864B.clear();
+         RSCG12864B.print_string_12_xy(40,25,nzh[xh]);
+         RSCG12864B.print_string_12_xy(55,25,nzm[xm]);
+      }      
+      else if(hpressed==1 && digitalRead(9)==1)      hpressed = 0;
+      if(mpressed==0 && digitalRead(10)==0)//设置时间
+      {
+         mpressed = 1;
+         xm+=5;
+         if(xm==60) xm=0;
+         RSCG12864B.clear();
+         RSCG12864B.print_string_12_xy(40,25,nzh[xh]);
+         RSCG12864B.print_string_12_xy(55,25,nzm[xm]);
+      }      
+      else if(mpressed==1 && digitalRead(10)==1)      tpressed = 0;
+      if(tpressed==0 && digitalRead(8)==0)//推出
+      {
+          tpressed = 1;
+          delay(50);
+          return; 
+      }      
+      else if(tpressed==1 && digitalRead(8)==1)      tpressed = 0;
+    }
+    
+}
 void loop()
 {
-  
+    if(tpressed==0 && digitalRead(8)==0)//进入闹钟
+    {
+         tpressed = 1;
+         JL_Set_Time();
+    }      
+    else if(tpressed==1 && digitalRead(8)==1)      tpressed = 0;
+
 }
